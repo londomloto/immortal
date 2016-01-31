027e9237752abb15fa908c2ea1cb8c8b82c243c0
londomloto <roso.sasongko@gmail.com>
2016-01-31 20:13:58 +0700

    change layout based on bootstrap

--- pages/layout.php
+++ pages/layout.php
@@ -1,40 +1,69 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title><?php echo get_config('title'); ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor/bootstrap/css/bootstrap.css">
+
 </head>
 <body>
 
-    <div class="container vbox">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Imortal</a>
+            </div>
 
-        <div class="header">
-            <h1>pushstate.com</h1>
-        </div>
-
-        <div class="flex hbox">
-
-            <div class="sidebar">
-                <ul class="menu">
-                    <li><a href="<?php echo site_url('home'); ?>" data-push="1">Homepage</a></li>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php echo site_url('home'); ?>" data-push="1">Home <span class="sr-only">(current)</span></a></li>
                     <li><a href="<?php echo site_url('products'); ?>" data-push="1">Products</a></li>
-                    <li><a href="<?php echo site_url('about'); ?>" data-push="1">About Us</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Components <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">One more separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
 
-                    <li><a href="http://www.facebook.com" target="_blank">Facebook</a></li>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administrator <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
-
-            <div class="flex tampungan">
-
-            	<?php echo get_content(); ?>
-
-            </div>
-
         </div>
+    </nav>
+
+    <div class="container tampungan">
+        <?php echo get_content(); ?>
     </div>
 
-
-    <script src="<?php echo base_url(); ?>js/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>vendor/jquery/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.js"></script>
     <script src="<?php echo base_url(); ?>js/script.js"></script>
 
 </body>
