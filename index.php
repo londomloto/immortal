3d73181b113781cdfed74f68c0f6f88503ab87d0
londomloto <roso.sasongko@gmail.com>
2016-02-04 05:42:24 +0700

    update error_reporting

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+error_reporting(E_ALL ^ E_DEPRECATED);
+ini_set('display_errors', 1);
+
+date_default_timezone_set('Asia/Jakarta');
+
 define('DS', DIRECTORY_SEPARATOR);
 define('BASEPATH', __DIR__.DS);
 
