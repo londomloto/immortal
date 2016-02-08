20756482f63507c326c2b247b13cdde129887159
londomloto <roso.sasongko@gmail.com>
2016-02-08 23:23:26 +0700

    separate database config

--- config.php
+++ config.php
@@ -1,37 +1,43 @@
 <?php
 
+switch($_SERVER['SERVER_ADMIN']) {
+
+	default:
+		$database = array(
+			'host' => 'localhost',
+			'user' => 'root',
+			'pass' => '',
+			'name' => 'immortal',
+			'load' => true
+		);
+	break;
+
+	case 'supernova@dev.local':
+		$database = array(
+			'host' => 'localhost',
+			'user' => 'root',
+			'pass' => 'secret',
+			'name' => 'immortal',
+			'load' => true
+		);
+	break;
+
+}
+
 return array(
-	
 	'name' => 'App',
-	
 	'version' => '1.0.0',
-	
 	'title' => 'immortal',
-	
-	'author' => 'supernova',
-	
+	'author' => 'supernova, arief',
 	'description' => 'simple php application',
-	
-	'keywords' => array('simple', 'php', 'application', 'procedural'),
-	
+	'keywords' => 'simple, php, application',
 	'index' => '',
-	
 	'default' => 'home',
-	
 	'suffix' => '.xyz',
-	
 	'autoload' => array(
 		'database',
 		'session',
 		'pagination'
 	),
-
-	'database'=> array(
-		'host' => 'localhost',
-		'user' => 'root',
-		'pass' => '',
-		'name' => 'immortal',
-		'load' => true
-	)
-
+	'database'=> $database
 );
