2185d17e5a06e3efd7dd8cd4c90f217052507315
londomloto <roso.sasongko@gmail.com>
2016-02-10 03:27:19 +0700

    tabs

--- config.php
+++ config.php
@@ -2,41 +2,41 @@
 
 switch($_SERVER['SERVER_ADMIN']) {
 
-	default:
-		$database = array(
-			'host' => 'localhost',
-			'user' => 'root',
-			'pass' => '',
-			'name' => 'immortal',
-			'load' => true
-		);
-	break;
+    default:
+        $database = array(
+            'host' => 'localhost',
+            'user' => 'root',
+            'pass' => '',
+            'name' => 'immortal',
+            'load' => true
+        );
+    break;
 
-	case 'supernova@dev.local':
-		$database = array(
-			'host' => 'localhost',
-			'user' => 'root',
-			'pass' => 'secret',
-			'name' => 'immortal',
-			'load' => true
-		);
-	break;
+    case 'supernova@dev.local':
+        $database = array(
+            'host' => 'localhost',
+            'user' => 'root',
+            'pass' => 'secret',
+            'name' => 'immortal',
+            'load' => true
+        );
+    break;
 
 }
 
 return array(
-	'name' => 'App',
-	'version' => '1.0.0',
-	'title' => 'immortal',
-	'author' => 'supernova, arief',
-	'description' => 'simple php application',
-	'keywords' => 'simple, php, application',
-	'index' => '',
-	'default' => 'home',
-	'suffix' => '.xyz',
-	'autoload' => array(
-		'database',
-		'pagination'
-	),
-	'database'=> $database
+    'name' => 'App',
+    'version' => '1.0.0',
+    'title' => 'immortal',
+    'author' => 'supernova, arief',
+    'description' => 'simple php application',
+    'keywords' => 'simple, php, application',
+    'index' => '',
+    'default' => 'home',
+    'suffix' => '.xyz',
+    'autoload' => array(
+        'database',
+        'pagination'
+    ),
+    'database'=> $database
 );
