e6ad6634cfefb8b632af295e94773f77f971e62e
londomloto <roso.sasongko@gmail.com>
2016-08-16 22:29:49 +0700

    update libs/translation.php

--- libs/translation.php
+++ libs/translation.php
@@ -2,7 +2,7 @@
 
 function load_textdomain() {
     $locale = 'INDONESIA'; // 'id_ID.UTF-8'; // get_config('locale');
-    var_dump(setlocale(LC_ALL, $locale));
+    //var_dump(setlocale(LC_ALL, $locale));
     bindtextdomain('app', LANGDIR);
     textdomain('app');
     bind_textdomain_codeset('app', 'UTF-8');
